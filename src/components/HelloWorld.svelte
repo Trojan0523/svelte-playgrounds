9b07fdbccef79cd639442ea1dcb5e639af0f3351
trojan0523 <1153532327@qq.com>
2024-04-16 11:41:28 +0800

    chore: init

---
+++ src/components/HelloWorld.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    export const name = "svelte";
+
+    let count = 1;
+
+    function increment() {
+        count += 1;
+    }
+</script>
+
+<h1>Hello {name}</h1>
+
+<button on:click={increment}>Click me ~ {count}</button>
+
+{#if count > 10}
+    <div>{count} is less than 10</div>
+{:else if count > 12}
+    <div>{count} is between 10 and 12</div>
+{:else}
+    <div>{count} is greater than 12</div>
+{/if}
